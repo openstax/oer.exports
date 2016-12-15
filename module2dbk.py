1db8bbc0db1d51786e400665ea23ae8714cee98e
Ross Reedstrom <reedstrm@gmail.com>
2016-12-15 10:26:30 -0600

    reindent to standard 4-spaces

--- module2dbk.py
+++ module2dbk.py
@@ -1,21 +1,21 @@
 """
 Copyright (c) 2013 Rice University
 
-This software is subject to the provisions of the GNU AFFERO GENERAL PUBLIC LICENSE Version 3.0 (AGPL).
+This software is subject to the provisions of the
+GNU AFFERO GENERAL PUBLIC LICENSE Version 3.0 (AGPL).
 See LICENSE.txt for details.
 """
 
 import sys
 import os
 try:
-  import Image
+    import Image
 except:
-  from PIL import Image
+    from PIL import Image
 from StringIO import StringIO
 import subprocess
 
 from lxml import etree
-import urllib2
 import util
 
 from saxon import Saxon
@@ -31,9 +31,11 @@ MATH2SVG_PATH = util.resource_filename('xslt2', 'math2svg-in-docbook.xsl')
 DOCBOOK_BOOK_XSL = util.makeXsl('moduledbk2book.xsl')
 
 MATH_XPATH = etree.XPath('//mml:math', namespaces=util.NAMESPACES)
-DOCBOOK_SVG_IMAGE_XPATH = etree.XPath('//db:imagedata[svg:svg]', namespaces=util.NAMESPACES)
+DOCBOOK_SVG_IMAGE_XPATH = etree.XPath('//db:imagedata[svg:svg]',
+                                      namespaces=util.NAMESPACES)
 DOCBOOK_SVG_XPATH = etree.XPath('svg:svg', namespaces=util.NAMESPACES)
-DOCBOOK_IMAGE_XPATH = etree.XPath('//db:imagedata[@fileref]', namespaces=util.NAMESPACES)
+DOCBOOK_IMAGE_XPATH = etree.XPath('//db:imagedata[@fileref]',
+                                  namespaces=util.NAMESPACES)
 
 # -----------------------------
 # Transform Structure:
@@ -49,216 +51,217 @@ DOCBOOK_IMAGE_XPATH = etree.XPath('//db:imagedata[@fileref]', namespaces=util.NA
 # - A list of log messages
 #
 
+
 def extractLog(entries):
-  """ Takes in an etree.xsl.error_log and returns a list of dicts (JSON) """
-  log = []
-  for entry in entries:
+    """ Takes in an etree.xsl.error_log and returns a list of dicts (JSON) """
+    log = []
+    for entry in entries:
     # Entries are of the form:
     # {'level':'ERROR','id':'id1234','msg':'Descriptive message'}
-    text = entry.message
-    if text:
-      print >> sys.stderr, text.encode('utf-8')
-    #try:
-    #    dict = json.loads(text)
-    #    errors.append(dict)
-    #except ValueError:
-    log.append({
-      u'level':u'CRITICAL',
-      u'id'   :u'(none)',
-      u'msg'  :unicode(text) })
+        text = entry.message
+        if text:
+            print >> sys.stderr, text.encode('utf-8')
+        #try:
+        #    dict = json.loads(text)
+        #    errors.append(dict)
+        #except ValueError:
+        log.append({
+          u'level':u'CRITICAL',
+          u'id'   :u'(none)',
+          u'msg'  :unicode(text) })
 
 def makeTransform(file):
-  xsl = util.makeXsl(file)
-  def t(xml, files, **params):
-    xml = xsl(xml, **params)
-    errors = extractLog(xsl.error_log)
-    return xml, {}, errors
-  return t
+    xsl = util.makeXsl(file)
+    def t(xml, files, **params):
+        xml = xsl(xml, **params)
+        errors = extractLog(xsl.error_log)
+        return xml, {}, errors
+    return t
 
 # Main method. Doing all steps for the Google Docs to CNXML transformation
 def convert(moduleId, xml, filesDict, collParams, temp_dir, svg2png=True, math2svg=True, reduce_quality=False):
-  """ Convert a cnxml file (and dictionary of filename:bytes) to a Docbook file and dict of filename:bytes) """
+    """ Convert a cnxml file (and dictionary of filename:bytes) to a Docbook file and dict of filename:bytes) """
 
-  #if 'index.included.dbk' in filesDict:
-  #  print >> sys.stderr, "LOG: Using already converted dbk file!"
-  #  return (filesDict['index.included.dbk'], {})
-  #print >> sys.stderr, "LOG: Working on Module %s" % moduleId
-  # params are XPaths so strings need to be quoted
-  params = {'cnx.module.id': "'%s'" % moduleId, 'cnx.svg.chunk': 'false'}
-  params.update(collParams)
+    #if 'index.included.dbk' in filesDict:
+    #  print >> sys.stderr, "LOG: Using already converted dbk file!"
+    #  return (filesDict['index.included.dbk'], {})
+    #print >> sys.stderr, "LOG: Working on Module %s" % moduleId
+    # params are XPaths so strings need to be quoted
+    params = {'cnx.module.id': "'%s'" % moduleId, 'cnx.svg.chunk': 'false'}
+    params.update(collParams)
 
-  def mathml2svg(xml, files, **params):
-      try: 
-          global parser
-          global sax
-          global mc
-          if sax is None:
-              sax = Saxon()
-      
-          if mc is None:
-              mc = memcache.Client(['127.0.0.1:11211'], debug=0)
-      
-          if not math2svg:
-              return xml, {}, []
-      
-          formularList = MATH_XPATH(xml)
-          math_list = []
-          svgs = {}
-      
-          unprocessed = {}
-      
-          for mathml in formularList:
-              mathml_key = hashlib.md5(etree.tostring(mathml)).hexdigest()
-              math_list.append((mathml, mathml_key))
-              svg_str = mc.get(mathml_key)
-              if svg_str:
-                  svgs[mathml_key] = svg_str
-              else:
-                  unprocessed[mathml_key] = mathml
-
-          if len(unprocessed) > 0:
-              mathml_str_list = [ etree.tostring(mathml) for mathml in unprocessed.values() ]
-              mathml_tree_str = "<root>" + ''.join(mathml_str_list) + "</root>"
-              mathml_svg_tree_str = sax.convert(mathml_tree_str)
-              mathml_svg_tree = etree.parse(StringIO(mathml_svg_tree_str),parser)
-              root = mathml_svg_tree.getroot()
-              mathml_svg_list = root.getchildren()
-              for  mathml_key, expected_mathml in unprocessed.items():
-                  svg = mathml_svg_list.pop(0)
-                  returned_mathml = mathml_svg_list.pop(0)
-                  if etree.tostring(returned_mathml) == etree.tostring(expected_mathml):
-                      svg_str = etree.tostring(svg)
-                      mc.set(mathml_key, svg_str)
-                      svgs[mathml_key] = svg_str
-                  else:
-                      raise ValueError("returned mathml not expected")
-
-          # All worked, now update XML tree
-          for mathml, math_key in math_list:
-              svg = etree.parse(StringIO(svgs[math_key]), parser).getroot()
-              mathml.addprevious(svg)
-
-      except RuntimeError:
-          formularList = MATH_XPATH(xml)
-          strErr = ''
-          if len(formularList) > 0:
-    
-            # Take XML from stdin and output to stdout
-            # -s:$DOCBOOK1 -xsl:$MATH2SVG_PATH -o:$DOCBOOK2
-            strCmd = ['java','-jar', SAXON_PATH, '-s:-', '-xsl:%s' % MATH2SVG_PATH]
-    
-            # run the program with subprocess and pipe the input and output to variables
-            p = subprocess.Popen(strCmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, close_fds=True)
-            # set STDIN and STDOUT and wait untill the program finishes
-            stdOut, strErr = p.communicate(etree.tostring(xml))
-    
-            #xml = etree.fromstring(stdOut, recover=True) # @xml:id is set to '' so we need a lax parser
-            parser = etree.XMLParser(recover=True)
-            xml = etree.parse(StringIO(stdOut), parser)
-    
-            if strErr:
-              print >> sys.stderr, strErr.encode('utf-8')                           
-      
-      return xml, {}, [] # xml, newFiles, log messages
-
-  def imageResize(xml, files, **params):
-    # TODO: parse the XML and xpath/annotate it as we go.
-    newFiles = {}
-    for position, image in enumerate(DOCBOOK_IMAGE_XPATH(xml)):
-      filename = image.get('fileref')
-      mimeType = image.getparent().get('format', 'JPEG')
-      # Exception thrown if image doesn't exist
-      if filename in files:
+    def mathml2svg(xml, files, **params):
         try:
-          bytes = files[filename]
-          img = Image.open(StringIO(bytes))
+            global parser
+            global sax
+            global mc
+            if sax is None:
+                sax = Saxon()
 
-          width = img.size[0]
-          height = img.size[1]
-          if reduce_quality: # Only resize when in DEBUG mode (so content entry sees the High Resolution PDFs)
-            print >> sys.stderr, 'LOG: DEBUG: Reducing quality of %s (%s)' % (filename, mimeType)
-            # Always resave
-            fname = "_autogen-png2jpeg-%04d.jpg" % (position + 1)
+            if mc is None:
+                mc = memcache.Client(['127.0.0.1:11211'], debug=0)
 
-            bytesFile = StringIO()
-            img.save(bytesFile, 'jpeg', optimize=True, quality=30)
-            # Since we probably changed the type of file to JPEG change the format in the image tag
-            # The image type is used in the epub manifest to map images to their mime-type
-            image.set('fileref', fname)
-            image.set('format', 'JPEG')
-            image.getparent().set('format', 'JPEG')
+            if not math2svg:
+                return xml, {}, []
 
-            newFiles[fname] = bytesFile.getvalue()
+            formularList = MATH_XPATH(xml)
+            math_list = []
+            svgs = {}
 
-        except IOError:
-          print >> sys.stderr, 'LOG: WARNING: Malformed image %s' % filename
-      else:
-        print >> sys.stderr, 'LOG: WARNING: Image missing %s' % filename
-        pass
-    return xml, newFiles, [] # xml, newFiles, log messages
+            unprocessed = {}
 
-  # Convert SVG elements to PNG files
-  # (this mutates the document)
-  def svg2pngTransform(xml, files, **params):
-    newFiles2 = {}
-    if svg2png:
-      for position, image in enumerate(DOCBOOK_SVG_IMAGE_XPATH(xml)):
-        print >> sys.stderr, 'LOG: Converting SVG to PNG'
-        # TODO add the generated file to the edited files dictionary
-        strImageName = "_autogen-svg2png-%04d.png" % (position + 1)
-        svg = DOCBOOK_SVG_XPATH(image)[0]
-        svgStr = etree.tostring(svg)
+            for mathml in formularList:
+                mathml_key = hashlib.md5(etree.tostring(mathml)).hexdigest()
+                math_list.append((mathml, mathml_key))
+                svg_str = mc.get(mathml_key)
+                if svg_str:
+                    svgs[mathml_key] = svg_str
+                else:
+                    unprocessed[mathml_key] = mathml
 
-        pngStr = util.svg2png(svgStr)
-        newFiles2[strImageName] = pngStr
-        image.set('fileref', strImageName)
-        image.set('format', 'PNG')
-        image.getparent().set('format', 'PNG')
+            if len(unprocessed) > 0:
+                mathml_str_list = [ etree.tostring(mathml) for mathml in unprocessed.values() ]
+                mathml_tree_str = "<root>" + ''.join(mathml_str_list) + "</root>"
+                mathml_svg_tree_str = sax.convert(mathml_tree_str)
+                mathml_svg_tree = etree.parse(StringIO(mathml_svg_tree_str),parser)
+                root = mathml_svg_tree.getroot()
+                mathml_svg_list = root.getchildren()
+                for  mathml_key, expected_mathml in unprocessed.items():
+                    svg = mathml_svg_list.pop(0)
+                    returned_mathml = mathml_svg_list.pop(0)
+                    if etree.tostring(returned_mathml) == etree.tostring(expected_mathml):
+                        svg_str = etree.tostring(svg)
+                        mc.set(mathml_key, svg_str)
+                        svgs[mathml_key] = svg_str
+                    else:
+                        raise ValueError("returned mathml not expected")
 
-    return xml, newFiles2, [] # xml, newFiles, log messages
+            # All worked, now update XML tree
+            for mathml, math_key in math_list:
+                svg = etree.parse(StringIO(svgs[math_key]), parser).getroot()
+                mathml.addprevious(svg)
 
-  PIPELINE = [
-    makeTransform('cnxml-clean.xsl'),
-    makeTransform('cnxml-clean-math.xsl'),
-    # Have to run the cleanup twice because we remove empty mml:mo,
-    # then remove mml:munder with only 1 child.
-    # See m21903
-    makeTransform('cnxml-clean-math.xsl'),
-    makeTransform('cnxml-clean-math-simplify.xsl'),   # Convert "simple" MathML to cnxml
-    makeTransform('cnxml2dbk.xsl'),   # Convert to docbook
-    mathml2svg,
-    makeTransform('dbk-clean.xsl'),
-    imageResize, # Resizing is done before svg2png because svg2png uses a reduced color depth
-    svg2pngTransform,
-    makeTransform('dbk-svg2png.xsl'), # Clean up the image attributes
-#    dbk2xhtml,
-  ]
+        except RuntimeError:
+            formularList = MATH_XPATH(xml)
+            strErr = ''
+            if len(formularList) > 0:
 
-  newFiles = {}
-  origAndNewFiles = {}
-  origAndNewFiles.update(filesDict)
+                # Take XML from stdin and output to stdout
+                # -s:$DOCBOOK1 -xsl:$MATH2SVG_PATH -o:$DOCBOOK2
+                strCmd = ['java','-jar', SAXON_PATH, '-s:-', '-xsl:%s' % MATH2SVG_PATH]
 
-  for transform in PIPELINE:
-    xml, newFiles2, errors = transform(xml, origAndNewFiles, **params)
-    newFiles.update(newFiles2)
-    origAndNewFiles.update(newFiles2)
+                # run the program with subprocess and pipe the input and output to variables
+                p = subprocess.Popen(strCmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, close_fds=True)
+                # set STDIN and STDOUT and wait untill the program finishes
+                stdOut, strErr = p.communicate(etree.tostring(xml))
 
-  origAndNewFiles.update(newFiles)
+                #xml = etree.fromstring(stdOut, recover=True) # @xml:id is set to '' so we need a lax parser
+                parser = etree.XMLParser(recover=True)
+                xml = etree.parse(StringIO(stdOut), parser)
 
-  # Write out all files to the temp dir so they don't stay in memory
-  for (key, value) in origAndNewFiles.items():
-    print >> sys.stderr, "Writing out " + os.path.join(temp_dir, key)
-    f = open(os.path.join(temp_dir, key), 'w')
-    f.write(value)
-    f.close()
-  newFiles = {}
+                if strErr:
+                    print >> sys.stderr, strErr.encode('utf-8')
 
-  # Create a standalone db:book file for the module
-  dbkStandalone = DOCBOOK_BOOK_XSL(xml)
-  newFiles['index.standalone.dbk'] = etree.tostring(dbkStandalone)
-  # uncomment this to write out individual docbook module files
-  #with open(os.path.join(temp_dir,'%s.dbk' % moduleId), 'w') as f:
-  #  f.write(newFiles['index.standalone.dbk'])
+        return xml, {}, [] # xml, newFiles, log messages
+
+    def imageResize(xml, files, **params):
+        # TODO: parse the XML and xpath/annotate it as we go.
+        newFiles = {}
+        for position, image in enumerate(DOCBOOK_IMAGE_XPATH(xml)):
+            filename = image.get('fileref')
+            mimeType = image.getparent().get('format', 'JPEG')
+            # Exception thrown if image doesn't exist
+            if filename in files:
+                try:
+                    bytes = files[filename]
+                    img = Image.open(StringIO(bytes))
+
+                    width = img.size[0]
+                    height = img.size[1]
+                    if reduce_quality: # Only resize when in DEBUG mode (so content entry sees the High Resolution PDFs)
+                        print >> sys.stderr, 'LOG: DEBUG: Reducing quality of %s (%s)' % (filename, mimeType)
+                        # Always resave
+                        fname = "_autogen-png2jpeg-%04d.jpg" % (position + 1)
+
+                        bytesFile = StringIO()
+                        img.save(bytesFile, 'jpeg', optimize=True, quality=30)
+                        # Since we probably changed the type of file to JPEG change the format in the image tag
+                        # The image type is used in the epub manifest to map images to their mime-type
+                        image.set('fileref', fname)
+                        image.set('format', 'JPEG')
+                        image.getparent().set('format', 'JPEG')
+
+                        newFiles[fname] = bytesFile.getvalue()
+
+                except IOError:
+                    print >> sys.stderr, 'LOG: WARNING: Malformed image %s' % filename
+            else:
+                print >> sys.stderr, 'LOG: WARNING: Image missing %s' % filename
+                pass
+        return xml, newFiles, [] # xml, newFiles, log messages
+
+    # Convert SVG elements to PNG files
+    # (this mutates the document)
+    def svg2pngTransform(xml, files, **params):
+        newFiles2 = {}
+        if svg2png:
+            for position, image in enumerate(DOCBOOK_SVG_IMAGE_XPATH(xml)):
+                print >> sys.stderr, 'LOG: Converting SVG to PNG'
+                # TODO add the generated file to the edited files dictionary
+                strImageName = "_autogen-svg2png-%04d.png" % (position + 1)
+                svg = DOCBOOK_SVG_XPATH(image)[0]
+                svgStr = etree.tostring(svg)
+
+                pngStr = util.svg2png(svgStr)
+                newFiles2[strImageName] = pngStr
+                image.set('fileref', strImageName)
+                image.set('format', 'PNG')
+                image.getparent().set('format', 'PNG')
+
+        return xml, newFiles2, [] # xml, newFiles, log messages
+
+    PIPELINE = [
+      makeTransform('cnxml-clean.xsl'),
+      makeTransform('cnxml-clean-math.xsl'),
+      # Have to run the cleanup twice because we remove empty mml:mo,
+      # then remove mml:munder with only 1 child.
+      # See m21903
+      makeTransform('cnxml-clean-math.xsl'),
+      makeTransform('cnxml-clean-math-simplify.xsl'),   # Convert "simple" MathML to cnxml
+      makeTransform('cnxml2dbk.xsl'),   # Convert to docbook
+      mathml2svg,
+      makeTransform('dbk-clean.xsl'),
+      imageResize, # Resizing is done before svg2png because svg2png uses a reduced color depth
+      svg2pngTransform,
+      makeTransform('dbk-svg2png.xsl'), # Clean up the image attributes
+  #    dbk2xhtml,
+    ]
+
+    newFiles = {}
+    origAndNewFiles = {}
+    origAndNewFiles.update(filesDict)
+
+    for transform in PIPELINE:
+        xml, newFiles2, errors = transform(xml, origAndNewFiles, **params)
+        newFiles.update(newFiles2)
+        origAndNewFiles.update(newFiles2)
+
+    origAndNewFiles.update(newFiles)
+
+    # Write out all files to the temp dir so they don't stay in memory
+    for (key, value) in origAndNewFiles.items():
+        print >> sys.stderr, "Writing out " + os.path.join(temp_dir, key)
+        f = open(os.path.join(temp_dir, key), 'w')
+        f.write(value)
+        f.close()
+    newFiles = {}
+
+    # Create a standalone db:book file for the module
+    dbkStandalone = DOCBOOK_BOOK_XSL(xml)
+    newFiles['index.standalone.dbk'] = etree.tostring(dbkStandalone)
+    # uncomment this to write out individual docbook module files
+    #with open(os.path.join(temp_dir,'%s.dbk' % moduleId), 'w') as f:
+    #  f.write(newFiles['index.standalone.dbk'])
 
 
-  return etree.tostring(xml), newFiles
+    return etree.tostring(xml), newFiles
